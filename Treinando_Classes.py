a9cdefa37549379b12486bca83aca42f2120e2aa
Sérgio Lima <sergiodelima22@gmail.com>
2023-05-04 18:19:32 -0300

    alteracao-2

---
+++ Treinando_Classes.py
@@ -0,0 +1,10 @@
+class Nome():
+    def __init__(self, nome):
+        self.nome = nome
+        print(f'{self.nome}', end='')
+    def SobreNome(self, sobrenome):
+        self.sobrenome = sobrenome
+        print(self.sobrenome)
+Pessoa = Nome('Sérgio')
+Pessoa.SobreNome(' dos Santos Lima')
+print(Pessoa.SobreNome)
