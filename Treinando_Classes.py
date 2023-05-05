6001d0a1f4093c3587a3272a1c51cb7738daf867
Sérgio Lima <sergiodelima22@gmail.com>
2023-05-05 16:17:15 -0300

    alteracao-2

--- Treinando_Classes.py
+++ Treinando_Classes.py
@@ -1,14 +1,16 @@
-class Nome():
-    def __init__(self, nome):
+class Personalidade():
+    def __init__(self, nome, sobrenome, idade):
         self.nome = nome
-        print(f'{self.nome}', end='')
-    def SobreNome(self, sobrenome):
         self.sobrenome = sobrenome
-        print(self.sobrenome)
-    def Idade(self, idade):
         self.idade = idade
-        print(f'Idade: {self.idade} anos')
-Pessoa = Nome('Sérgio')
-Pessoa.SobreNome(' Lima')
-Pessoa.Idade('32')
-print(Pessoa.SobreNome)
+
+Pessoa = Personalidade('Sérgio',' dos Santos Lima', '32 anos')
+print(f'{Pessoa.nome}', end='')
+print(Pessoa.sobrenome)
+print(f'Idade: {Pessoa.idade}')
+
+Pessoa2 = Personalidade('Antônio',' Veranês', '26 anos')
+print(f'{Pessoa2.nome}', end='')
+print(Pessoa2.sobrenome)
+print(f'Idade: {Pessoa2.idade}')
+        
