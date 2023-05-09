bf6edcc5941460aca167431e42298ff9596f97cd
Sérgio Lima <sergiodelima22@gmail.com>
2023-05-08 23:30:28 -0300

    acrescimo-de-metodo-escolha-de-producao-cinematografica

--- Netflix.py
+++ Netflix.py
@@ -4,28 +4,24 @@ class cliente():
         self.idade = idade
         self.sexo = sexo
     
-    def escolherFilmeSerie(serie):
-        if serie == 1:
+    def escolherFilmeSerie(self, producao):
+        if producao == 1:
             print('The Chosen')
-        elif serie == 2:
+        elif producao == 2:
             print('Deus Não Está Morto 3')
-        elif serie == 3:
-            print('Five Nights At Freddy')
-        else:
-            print('Opção inválida! Tente novamente!')    
+        elif producao == 3:
+            print('Five Nights At Freddy')        
 
 NomeCliente = str(input('Qual seu nome? > '))
 IdadeCliente = int(input('Qual é a sua idade? > '))
 SexoCLiente = str(input('Qual é o seu sexo? > '))
-Escolha = int(input('Escolha uma das produções cinetográticas abaixo pelo número: > '))
 print('1) The Chosen')
 print('2) Deus Não Está Morto 3')
 print('3) Five Nights At Freddy')
-Cliente = cliente(NomeCliente, IdadeCliente, SexoCLiente)
-cliente.escolherFilmeSerie(serie=Escolha)
-print(f'Nome: {Cliente.nome}')
-print(f'Idade: {Cliente.idade}')
-print(f'Sexo: {Cliente.sexo}')
-print(cliente.escolherFilmeSerie)
-
+Escolha = int(input('Escolha uma das produções cinetográticas acima pelo número: > '))
+Cliente1 = cliente(NomeCliente, IdadeCliente, SexoCLiente)
+print(f'Nome: {Cliente1.nome}')
+print(f'Idade: {Cliente1.idade}')
+print(f'Sexo: {Cliente1.sexo}')
+Cliente1.escolherFilmeSerie(Escolha)
         
